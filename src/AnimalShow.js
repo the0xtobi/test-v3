767dd38938cbc2d8895b1efa468f379797f2bae0
blur <thetobiterra@gmail.com>
2023-01-21 18:46:42 +0100

    build

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,6 +1,30 @@
-function AnimalShow({type}){
-    return(
-        <div>{type}</div>
+import { useState } from 'react'
+
+import bird from './svg/bird.svg'
+import cat from './svg/cat.svg'
+import cow from './svg/cow.svg'
+import dog from './svg/dog.svg'
+import heart from './svg/heart.svg'
+import gator from './svg/gator.svg'
+import horse from './svg/horse.svg'
+
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    horse
+}
+
+function AnimalShow({ type }) {
+    const [clicks, setClicks] = useState(0)
+
+    return (
+        <div onClick={() => setClicks(clicks + 1)}>
+            <img alt='animal' src={svgMap[type]} />
+            <img alt='heart' src={heart} style={{ width: 10 + 10 * clicks + 'px' }} />
+        </div>
     )
 }
 
